afdb83e1d32eeb601986e8e4e4148988493b9bfa
Andrew Hilts <andrewchilts@gmail.com>
2016-11-10 12:02:54 -0500

    basic rtl support

--- canvasDoc.js
+++ canvasDoc.js
@@ -34,13 +34,11 @@ function Page(pageSize, margins, dpiFactor){
 		destinationCanvas.width = pageSize.width;
 		destinationCanvas.height = pageSize.height;
 		destinationCanvas.style = page.canvas.style;
-
 		destCtx = destinationCanvas.getContext('2d');
 
 		//create a rectangle with the desired color
 		destCtx.fillStyle = "#ffffff";
 		destCtx.fillRect(0,0,pageSize.width,pageSize.height);
-
 		//draw the original canvas onto the destination canvas
 		destCtx.drawImage(page.canvas, 0, 0);
 		page.canvas = destinationCanvas;
@@ -49,7 +47,7 @@ function Page(pageSize, margins, dpiFactor){
 	page.canvas.style.height = pageSize.height/dpiFactor/4 + 'px';
 	}
 
-	page.wrapText = function(text, options) {
+	page.wrapText = function(text, options, dir) {
 		var continuing = false;
 		if(typeof text == "string"){
 			var words = textToWords(text);
@@ -58,8 +56,10 @@ function Page(pageSize, margins, dpiFactor){
 			var words = text;
 			continuing = true;
 		}
+
+		startX = this.paintPosition.x;
+
 		var maxWidth = page.maxPaintPosition.x;
-		var xPos = this.paintPosition.x;
 
 		var line = '';
 
@@ -70,13 +70,23 @@ function Page(pageSize, margins, dpiFactor){
 			if(options.bulletItem && !continuing){
 				bulletItem = true;
 				line = "•   ";
+				if(dir == "rtl"){
+					startX -= 45;
+				}
+				else{
+					startX += 45;
+				}
 				maxWidth -= 45;
-				xPos += 45;
 			}
 			if(options.bulletItem && continuing){
 				bulletItem = true;
+				if(dir == "rtl"){
+					startX -= 45+40;
+				}
+				else{
+					startX += 45+40;
+				}
 				maxWidth -= 45+40;
-				xPos += 45+40;
 			}
 			if(options.noBottomMargin){
 				bottomMargin = page.lineHeight;
@@ -85,31 +95,61 @@ function Page(pageSize, margins, dpiFactor){
 		firstNewLineDone = false;
 
 		for(var n = 0; n < words.length; n++) {
-			var testLine = line + words[n] + '';
+			var testLine = line + words[n];
 			var metrics = this.ctx.measureText(testLine);
 			var testWidth = metrics.width;
-			if (testWidth > maxWidth && n > 0) {
-				this.ctx.fillText(line, xPos, this.paintPosition.y);
-		
-				if(bulletItem && !firstNewLineDone && !continuing){
+			var lineDone;
 
+			// Test if we have additional lines after this one.
+			if (testWidth > maxWidth && n > 0) {
+				rectStart = startX;
+				if(dir == "rtl"){
+					startX = maxWidth + this.paintPosition.x;
+					rectStart = this.paintPosition.x;
+					this.ctx.textAlign = "right"
+					this.ctx.direction = "rtl";
+				}
+				// this.ctx.fillStyle = '#ccc';
+				// this.ctx.fillRect(rectStart, this.paintPosition.y-page.lineHeight, maxWidth, page.lineHeight);
+				// this.ctx.fillStyle = '#000';
+				this.ctx.fillText(line, startX, this.paintPosition.y);
+				this.ctx.textAlign = "left";
+				this.ctx.direction = "ltr";
+
+				if(bulletItem && !firstNewLineDone && !continuing){
+					if(dir == "rtl"){
+						startX -= 42;
+					}
+					else{
+						startX += 42;
+					}
 					maxWidth -= 42;
-					xPos += 42;
 					firstNewLineDone = true;
 				}
-				line = words[n] + '';
+				line = words[n];
 				this.paintPosition.y += page.lineHeight;
 			}
 			else {
-	
 				line = testLine;
 			}
 			if(this.paintPosition.y >= page.maxPaintPosition.y){
 				return words.slice(n);
 			}
 		}
-		this.ctx.fillText(line, xPos, this.paintPosition.y);
+		rectStart = this.paintPosition.x;
+		if(dir == "rtl"){
+			startX = maxWidth + this.paintPosition.x;
+			this.ctx.textAlign = "right"
+			this.ctx.direction = "rtl";
+		}
+		// this.ctx.fillStyle = '#ccc';
+		// this.ctx.fillRect(rectStart, this.paintPosition.y-page.lineHeight, maxWidth, page.lineHeight);
+		// this.ctx.fillStyle = '#000';
+		// console.log(line);
+		this.ctx.fillText(line, startX, this.paintPosition.y);
 		this.paintPosition.y += bottomMargin;
+		this.ctx.textAlign = "left";
+		this.ctx.direction = "ltr";
 		return null;
 	}
 
@@ -169,9 +209,9 @@ function Document(paperType, margins){
 	// Add first page
 	self.pages.push(new Page(self.pageSize, self.margins, dpiFactor));
 
-	self.writeText = function(text, options){
+	self.writeText = function(text, options, dir){
 		activePage = self.pages[self.getActivePageIndex()];
-		text = activePage.wrapText(text, options);
+		text = activePage.wrapText(text, options, dir);
 		if(text == null){
 			return;
 		}
@@ -179,7 +219,7 @@ function Document(paperType, margins){
 			// Add new page, and keep writing
 			self.pages.push(new Page(self.pageSize, self.margins, dpiFactor));
 			self.activePageIndex += 1;
-			self.writeText(text, options);
+			self.writeText(text, options, dir);
 		}
 	}
 
@@ -215,9 +255,17 @@ function Document(paperType, margins){
 		for(var i=0; i < nodes.length; i++){
 			// Check if leaf node
 			if(nodes[i].el.children.length === 0 && nodes[i].el.innerText.length > 0){
+				style = getComputedStyle(nodes[i].el);
+				if(typeof style.direction !== "undefined"){
+					dir = style.direction;
+				}
+				else{
+					dir = "ltr";
+				}
 				pdfContent.push({
 					'tag': nodes[i].el.tagName,
-					'text': nodes[i].el.innerText
+					'text': nodes[i].el.innerText,
+					'dir': dir
 				});
 			}
 		}
@@ -240,7 +288,7 @@ function Document(paperType, margins){
 	self.writeHTMLtoDoc = function(el){
 		pdfContent = self.parseEl(el);
 		for(var i=0; i < pdfContent.length; i++){
-			self.writeText(pdfContent[i].text, pdfContent[i].options);
+			self.writeText(pdfContent[i].text, pdfContent[i].options, pdfContent[i].dir);
 		}
 		for (var i = 0; i < self.pages.length; i++) {
 			self.pages[i].addBackground();
